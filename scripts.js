b9d21b6c4f064de47bb3e4eee2b882478924275a
Anantha Krishnan PTA <ananthakrishnanpta@gmail.com>
2024-03-25 12:30:46 +0530

    added function expressions

--- scripts.js
+++ scripts.js
@@ -8,11 +8,41 @@ let number = document.getElementById("num-box") // accessing the division for di
 count_button.addEventListener("click", counter_func); 
 
 // definition of 'counter_func' function
+// This is a function definition. In javascript, these types of functions can be called before declaration.
 function counter_func() {
 
     // the below code accesses the inner text content of the division showing the number
     // parseInt() function converts the received value into an integer value
     // because by default, javascript won't recognize that the text is a number.
 
-    number.innerText = parseInt(number.innerText) + 1
+    number.innerText = parseInt(number.innerText) + 1;
+
 }
+
+// function declaration
+
+function square_var(x,y) {
+    return ((x**2) + (2*x*y) + (y**2));
+}
+
+// Since the above function only has one expression, we can use other types of functions:
+
+// 1. anonymous functions
+
+var square_var_1 = function(x,y) { return ((x**2) + (2*x*y) + (y**2)) };
+
+// 2. arrow functions 
+
+var square_var_2 = (x,y) => { return ((x**2) + (2*x*y) + (y**2)) }
+
+// The above 2 functions can be called 'function expression'
+// These functions can have multiple arguments but only one expression
+
+document.write("<h3><strong>Following is the output of the function calls of the square functions </strong></h3>")
+document.write(square_var(3,3));
+document.write("<br>")
+document.write(square_var_1(2,4));
+document.write("<br>")
+document.write(square_var_2(2,4));
+
+
